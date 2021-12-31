91337ab0b6a8c73f66ef1f90a915ca5d91762698
subin4256 <subin4256@naver.com>
2021-12-31 18:13:10 +0900

    21.12.31 약간 수정

--- 19webCrawling02.py
+++ 19webCrawling02.py
@@ -1,4 +1,25 @@
 
+# 모듈 임포트
+import pymysql
+
+# DB 연결
+conn = pymysql.connect(host='localhost',user='kosmo_user',
+                        password='1234', db='kosmo_db', charset='utf8')
+curs = conn.cursor()
+
+sql = f"""INSERT INTO board (title, content, id)
+        VALUES ('{input('타자기록:')}', kosmo)"""
+
+try:
+    curs.execute(sql)
+    conn.commit()
+    print("1개의 레코드가 입력됨")
+except Exception as e :
+    conn.rollback()
+    print("쿼리 실행시 오류발생", e)
+
+conn.close()
+
 import requests
 from bs4 import BeautifulSoup
 
